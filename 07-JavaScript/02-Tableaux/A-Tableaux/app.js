6a05c09f0f3f2af8c83dbbe7f0100b18a04bb080
Kernel-rb <matabsaifeddine@gmail.com>
2024-01-25 18:51:38 +0100

    update

---
+++ 07-JavaScript/02-Tableaux/A-Tableaux/app.js
@@ -0,0 +1,10 @@
+const arr = [1, 2, 3, 4, 5];
+const arr2 = ["a", "b", "c", "d", "e"];
+const arr3 = [1, "a", "b", 2, 3];
+const multiDim = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+const arrObj = [{ name: "Kernel" }, { name: "Mikasse" }, { name: "Saif" }];
+
+// console.log(arr);
+console.log(arrObj[0].name);
+console.log(arr.length);
+console.log(arr2.at(-1));
