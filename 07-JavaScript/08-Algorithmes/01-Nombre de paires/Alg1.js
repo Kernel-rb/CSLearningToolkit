77d08800dc5f2b86b60d58b5c9121c65effcaa44
Kernel-rb <matabsaifeddine@gmail.com>
2024-02-05 06:51:23 +0100

    Parité arr algo done :)

--- 07-JavaScript/08-Algorithmes/01-Nombre de paires/Alg1.js
+++ 07-JavaScript/08-Algorithmes/01-Nombre de paires/Alg1.js
@@ -1,11 +1,17 @@
-function add(n) {
-    somme = 0;
-    for(let i = 1; i <= n; i++) {
-        if (i % 2 === 0) {
-            somme += i;
+// i++ => i = i+ 1
+//  === => l9iyam  data type
+
+// n appartien à N  ([1, inf]) , si n est pair => n = 2 * k ( n % 2 = 0)
+
+function somme_des_nmbr_paire(n){
+    let somme = 0;
+    for(let i = 1; i <= n; i++){
+        if(i % 2 === 0){
+
+            somme = somme + i;
         }
     }
-    console.log(somme);
+    return `Valeur de n est ${n} et la somme des nombres pairs est ${somme}`;
 }
-
-add(6);
+console.log(somme_des_nmbr_paire(5))
+console.log(somme_des_nmbr_paire(10))
--- 07-JavaScript/08-Algorithmes/10-Parité/algo10.js
+++ 07-JavaScript/08-Algorithmes/10-Parité/algo10.js
@@ -1,12 +1,18 @@
-function algorithme() {
-    arr = [1, 2,1,4]
-    len = arr.length;
-    for (i = 0; i < len; i++) {
-        if (arr[i] % 2 == 0) {
-            arr[i] = "pair";
-        } else {
-            arr[i] = "impair";
-        }
+// khsna wa7d lfun li an3tiwha wa7d  arr[int]
+// khsha t khrj lina wa7d output :
+//  ila kan arr[i] => pair , khs treturni lina pair
+// ila kan arr[i] => impair , khs tretruni lina impair
+// flkher khs ikon 3ndna wa7d arr = ["pair" , "impair" , "pair"]
+
+function array(arr){
+    const resultat = [];
+    for(let number in arr){
+        const parite = ( number % 2 === 0)  ? "pair" : "impair";
+        const sntx  = `${arr[number]} : ${parite}`;
+        resultat.push(sntx);
     }
-    
+    return resultat
 }
+
+let testArr = [4,77]
+console.log(array(testArr))
