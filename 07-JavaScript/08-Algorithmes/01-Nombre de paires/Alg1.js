8793a77f175680b760b0bad27508b2326c7ab4e0
Kernel-rb <matabsaifeddine@gmail.com>
2024-02-06 12:00:03 +0100

    done for the 3rd time

--- 07-JavaScript/08-Algorithmes/01-Nombre de paires/Alg1.js
+++ 07-JavaScript/08-Algorithmes/01-Nombre de paires/Alg1.js
@@ -1,17 +1,12 @@
-// i++ => i = i+ 1
-//  === => l9iyam  data type
-
-// n appartien à N  ([1, inf]) , si n est pair => n = 2 * k ( n % 2 = 0)
-
-function somme_des_nmbr_paire(n){
+function nombre_paire(n) {
     let somme = 0;
-    for(let i = 1; i <= n; i++){
-        if(i % 2 === 0){
-
-            somme = somme + i;
+    for (let i = 1; i <= n; i++){
+        if (i % 2 === 0) {
+            somme += i;
         }
     }
-    return `Valeur de n est ${n} et la somme des nombres pairs est ${somme}`;
+    return somme
 }
-console.log(somme_des_nmbr_paire(5))
-console.log(somme_des_nmbr_paire(10))
+console.log(nombre_paire(6))
+console.log(nombre_paire(7))
+console.log(nombre_paire(8))
