9aecb6a3022c67a35182eb9bc62f4850010c9ad4
Kernel-rb <matabsaifeddine@gmail.com>
2024-02-14 18:10:23 +0100

    Chapitre 1 done

---
+++ 10-Java/src/chapitre-1/Quiz/displayGivenPhrase.java
@@ -0,0 +1,10 @@
+package Quiz;
+
+public class displayGivenPhrase {
+    public static void main(String[] args) {
+        System.out.println("to be or");
+        System.out.print("not to be ");
+
+    }
+    
+}
