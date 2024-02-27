3977eb2ecde069306953d4017be8ba1f458e8563
Kernel-rb <matabsaifeddine@gmail.com>
2024-02-27 15:10:27 +0100

    added Array of Struct

--- 04-C/Cours/04-Complex Data Types/06-Structures/arrayOfStruct.c
+++ 04-C/Cours/04-Complex Data Types/06-Structures/arrayOfStruct.c
@@ -15,4 +15,13 @@ int main ()
         myPoints[i].y = i;
         myPoints[i].z = i;
     }
+
+    for (i = 0 ; i < 10 ; i++){
+        printf("myPoint[%d].x = %d \n" , i , myPoints[i].x);
+        printf("myPoint[%d].y = %d \n" , i , myPoints[i].y);
+        printf("myPoint[%d].z = %d \n" , i , myPoints[i].z);
+
+    }
+
+    return 0;
 }
