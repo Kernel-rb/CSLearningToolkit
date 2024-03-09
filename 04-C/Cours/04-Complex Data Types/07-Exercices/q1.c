a135dcc6b7fd8c6d12b444d0b0769829cec84dbc
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-09 12:35:21 +0100

    update Matrix

---
+++ 04-C/Cours/04-Complex Data Types/07-Exercices/q1.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#define MAXDATA 1024
+
+typedef struct {
+  double data[MAXDATA];
+  int nrows;
+  int ncols;
+} Matrix;
+
+void printmat(Matrix M);
+
+
+Matrix matrixmult(Matrix A, Matrix B);
+
+int main(int argc, char *argv[])
+{
+  Matrix A = { {1.2, 2.3,
+                3.4, 4.5,
+                5.6, 6.7},
+               3,
+               2};
+  Matrix B = { {5.5, 6.6, 7.7,
+                1.2, 2.1, 3.3},
+               2,
+               3}; 
+  printmat(A);
+  printmat(B);
+
+   Matrix C = matrixmult(A, B);
+   printmat(C);
+
+  return 0;
+}
+
+
+
+// fnct pour afficher la matrice 
+void printmat(Matrix M)
+{
+  int i, j;
+  printf("[\n");
+  for (i = 0; i < M.nrows; i++) {
+    for (j = 0; j < M.ncols; j++) {
+      printf("%lf ", M.data[i * M.ncols + j]);
+    }
+    printf("\n");
+  }
+   printf("]\n\n");
+}
+
+
+
+// fnct pour la multiplication de 2 matrices
+Matrix matrixmult(Matrix A, Matrix B)
+{
+  
+}
+
