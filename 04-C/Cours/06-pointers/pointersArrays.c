1a6a9c67a769dc17e2308ae4f06d00ae0240c9e0
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-14 16:46:12 +0100

    Update pointer.exe

---
+++ 04-C/Cours/06-pointers/pointersArrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int *vec;
+    vec = (int*)malloc(sizeof(int)*3);
+    vec[0] = 1;
+    vec[1] = 2;
+    vec[2] = 3;
+    printf("vec[2] => %d \n" , *(vec+2));
+    free(vec);
+    return 0;
+}
