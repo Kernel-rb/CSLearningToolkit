c035461143f751f24752b83de114585ecd570dc1
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-15 16:09:27 +0100

    Fix double function call in PassingByReference.c

--- 04-C/Cours/06-pointers/PassingByReference.c
+++ 04-C/Cours/06-pointers/PassingByReference.c
@@ -9,7 +9,7 @@ int main(void)
 {
     int y = 3 ; 
     printf("the value of y = %d \n" , y);
-    double(&y);
+    double(&y); // adress of y
     printf("the new value after the double func , y = %d /n" , y);
     return 0;
 }
