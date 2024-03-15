d1e0678803a874231d2547e6dfb3f1b97efb4b9f
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-15 16:08:27 +0100

    Passing By Value vs Passing By Reference

---
+++ 04-C/Cours/06-pointers/PassingByReference.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void double(int *x)
+{
+    *x = *x * 2;
+}
+
+int main(void)
+{
+    int y = 3 ; 
+    printf("the value of y = %d \n" , y);
+    double(&y);
+    printf("the new value after the double func , y = %d /n" , y);
+    return 0;
+}
