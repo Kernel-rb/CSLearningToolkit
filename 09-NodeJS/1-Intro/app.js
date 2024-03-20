dc99c2afad41d257e2260234f21028c1ffc3bc77
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-20 13:33:31 +0100

    add comments

--- 09-NodeJS/1-Intro/app.js
+++ 09-NodeJS/1-Intro/app.js
@@ -1,10 +1,14 @@
 function lateFunc()
+//  function that will be executed after 1 second
 {
     console.log("this was done asynchronously")
+    // log that will be printed after 1 second
 }
 
-setTimeout(lateFunc, 1000);
 console.log("this the first log");
+
+setTimeout(lateFunc, 1000);
+// function that will be executed after 1 second
 
 
 console.log("this the last log");
