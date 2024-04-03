23acadb5d69db22545d55563a76fdd0eb2ebdc56
Kernel-rb <matabsaifeddine@gmail.com>
2024-04-03 13:31:33 +0100

    Char-data-types done 🦀

---
+++ 13-Rust/1-Cours/1.Primitve-Data-types/i-Char-data-types/main.rs
@@ -0,0 +1,7 @@
+fn main(){
+    let letter = 'a';
+    let digit = '1';
+    let finger = '\u{261D}'; // Unicode for the index finger emoji
+    let smiley = '\u{1F603}'; // Unicode for the smiling face with open mouth emoji
+    println!("{} \n {} \n {} \n {}", letter, digit, finger, smiley);
+}
---
+++ 13-Rust/1-Cours/1.Primitve-Data-types/i-Char-data-types/notes.md
@@ -0,0 +1,5 @@
+# Char data type 
+    - Represents a single character
+    - Unicode scalar value , that mean it can represent any character in the Unicode standard
+    - Stored using 4 bytes
+    
