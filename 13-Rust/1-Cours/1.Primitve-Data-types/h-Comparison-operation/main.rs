769dc2119882ff9d2ce3875d2eaed555f6f8382b
Kernel-rb <matabsaifeddine@gmail.com>
2024-04-03 13:23:46 +0100

    Comparison-operation done 🦀

---
+++ 13-Rust/1-Cours/1.Primitve-Data-types/h-Comparison-operation/main.rs
@@ -0,0 +1,11 @@
+fn main(){
+    let a = 1;
+    let b = 2;
+    println!("a : {}  and b : {}", a, b);
+    println!("a == b : {}", a == b);
+    println!("a != b : {}", a != b);
+    println!("a < b : {}", a < b);
+    println!("a > b : {}", a > b);
+    println!("a <= b : {}", a <= b);
+    println!("a >= b : {}", a >= b);
+}
---
+++ 13-Rust/1-Cours/1.Primitve-Data-types/h-Comparison-operation/notes.md
@@ -0,0 +1,8 @@
+# Equal signe  in Rust 
+        - `==` : Equal signe
+        - `!=` : Not equal signe
+        - `>` : Greater than signe
+        - `<` : Less than signe
+        - `>=` : Greater than or equal signe
+        - `<=` : Less than or equal signe
+        
