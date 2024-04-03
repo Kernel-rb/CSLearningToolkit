0248492cfd353ed0660c005c69e482e51d656123
Kernel-rb <matabsaifeddine@gmail.com>
2024-04-03 17:45:37 +0100

    Conditionnal-execution  done 🦀

---
+++ 13-Rust/1-Cours/4.Program Flow Control/a.Conditionnal-execution/main.rs
@@ -0,0 +1,11 @@
+fn main(){
+    if_condition();   
+}
+
+
+fn if_condition(){
+    let x:i8 = 3;
+    if x ==  3{
+        println!("x is equal to 3");
+    }
+}
---
+++ 13-Rust/1-Cours/4.Program Flow Control/a.Conditionnal-execution/notes.md
@@ -0,0 +1,3 @@
+# If Expressions
+    - Control which code gets executed depending on the conditions.
+    
