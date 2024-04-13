8b438bbc4368d5c7867e7add285b9716e2f4bb59
Kernel-rb <matabsaifeddine@gmail.com>
2024-04-13 16:31:10 +0100

    Condtionnal Statement  done ☕

--- 11-Java/cours/FreeCodeCamp/06.Conditional-Statement/ageCheaker.java
+++ 11-Java/cours/FreeCodeCamp/06.Conditional-Statement/ageCheaker.java
@@ -12,12 +12,13 @@ public class ageCheaker {
         System.out.printf("Hy %s , how old are u : " , name);
         int age = scanner.nextInt();
 
-        if(age >=  18 ){
+        if(age > 18 ){
             System.out.printf("Your are welcome , %s" , name);
+        }else if(age == 18){
+            System.out.printf("You are just 18 , %s" , name);
         }else{
-            System.out.printf("We  are sorry  %s  but we can't let you enter , your gonne wait %d years :( " , name , (18 - age));
+            System.out.printf("You are not allowed , %s  but you can try after %d years" , name , 18 - age);
         }
-    
         scanner.close();
     }  
 }
