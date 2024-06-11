fc19c89737359942533196fb675fac3943256295
Kernel-rb <matabsaifeddine@gmail.com>
2024-06-11 20:09:16 +0200

    @LL/prepend

--- 03-DataStructure/1-UdemyCourse/3-LinkedList/LL.py
+++ 03-DataStructure/1-UdemyCourse/3-LinkedList/LL.py
@@ -45,12 +45,11 @@ class LinkedList  :
         return temp # v
     def  prepend(self , value) :
         new_node = Node(value) 
-        if self.length != 0  : 
+        if self.length == 0  : 
+            self.head = new_node 
+            self.tail = new_node            
+        else : 
             self.head.next = self.head   
             self.head = new_node
-        else : 
-            self.head = new_node 
-            self.tail = new_node
-            
         self.length += 1
         return True 
