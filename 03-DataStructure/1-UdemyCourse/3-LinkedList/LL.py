e895c6f55356c8c7c80d5aab5823f5526d71d6a5
Kernel-rb <matabsaifeddine@gmail.com>
2024-06-11 20:45:11 +0200

    @LL/get(self,index)

--- 03-DataStructure/1-UdemyCourse/3-LinkedList/LL.py
+++ 03-DataStructure/1-UdemyCourse/3-LinkedList/LL.py
@@ -63,6 +63,11 @@ class LinkedList  :
             self.length -= 1
             if self.length  == 0  :
                 self.tail = None 
-                
         return temp
-    
+    def get(self , index ): 
+        if index <  0   or index >= self.length : 
+            return None
+        temp = self.head
+        for _ in range(index) : 
+            temp = temp.next
+        return temp
