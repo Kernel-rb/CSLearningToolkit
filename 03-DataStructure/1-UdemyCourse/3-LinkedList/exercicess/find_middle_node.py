ac1b21e66ad73add837e63fe83dcccda835ca987
Kernel-rb <matabsaifeddine@gmail.com>
2024-06-12 14:58:05 +0200

    @LL/exercices/find_middle_node.py

---
+++ 03-DataStructure/1-UdemyCourse/3-LinkedList/exercicess/find_middle_node.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+    def find_middle_node(self):
+        length = 0
+        current_node = self.head
+        while current_node is not None:
+            length += 1
+            current_node = current_node.next
+        midd = length // 2 
+        temp = self.head
+        for _ in range(midd):
+            temp = temp.next
+        return temp
+    
+
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+print( my_linked_list.find_middle_node().value )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    3
+    
+"""
